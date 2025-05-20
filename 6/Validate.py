f53a16fe1e5e0dd2cc2c310b262e8beb0de27f94
abc-def-ghi <abcdpqrstuvwxyz9876543201@gmail.com>
2025-05-21 01:37:31 +0530

    Update Validate.py

--- 6/Validate.py
+++ 6/Validate.py
@@ -3,18 +3,22 @@ from lxml import etree
 # Validate with DTD 
 xml_file = "Bookstore.xml" 
 dtd_file = "Bookstore.dtd" 
-xml_doc = etree.parse(xml_file) 
+xml_doc = etree.parse(xml_file)
+root = xml_doc.getroot()
 dtd = etree.DTD(dtd_file) 
 if dtd.validate(xml_doc): 
-    print("XML is valid according to DTD") 
+    print("XML is valid according to DTD")
+    print(etree.tostring(root, pretty_print=True).decode())
 else: 
     print("DTD Validation failed:", dtd.error_log.filter_from_errors()) 
  
 # Validate with XSD 
 xsd_file = "Bookstore.xsd" 
-xmlschema_doc = etree.parse(xsd_file) 
+xmlschema_doc = etree.parse(xsd_file)
+root1 = xmlschema_doc.getroot()
 xmlschema = etree.XMLSchema(xmlschema_doc)
 if xmlschema.validate(xml_doc): 
-    print("XML is valid according to XSD") 
+    print("XML is valid according to XSD")
+    print(etree.tostring(root1, pretty_print=True).decode())
 else: 
     print("XSD Validation failed:", xmlschema.error_log.filter_from_errors())
